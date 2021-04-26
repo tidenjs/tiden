8b68f74284b70a804b83a806a2829162a0f1d612
Mika <mika@wappia.se>
2021-04-26 13:19:15 +0200

    Missed removing create from cli

--- cli.js
+++ cli.js
@@ -1,11 +1,8 @@
-import create from "./cli/create.js"
 import init from "./cli/init.js"
 
 const [_node, _file, verb, ...args] = process.argv
 
-if (verb === `create`) {
-  create(noun, ...args)
-} else if (verb === `init`) {
+if (verb === `init`) {
   init()
 } else {
   throw new Error(`No such verb '${verb}'`)
