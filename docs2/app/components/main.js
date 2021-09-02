0266c67f0e721a7271026db44f5b17bbe6311a5f
anatolik <eastsorrowo1212@gmail.com>
2021-09-02 14:53:00 +0300

    conflict fixes

--- docs2/app/components/main.js
+++ docs2/app/components/main.js
@@ -1,17 +1,11 @@
-import { component, html } from "tiden"
+import { component, render} from "tiden"
 
 import css from "./main/css.js"
 import './apiItem.js'
 
 component(`x-main`, { css }, function main({ method }) {
-  return html`
-    <h1>${method.name}</h1>
 
-    ${method.markup.map(c => {
-      const [tag, text] = Object.entries(c)[0]
-      return html`
-      <x-api-item .tag="${tag}" .text="${text}"></x-api-item>
-    `}
-  )}
-  `
+  //useEffect and this
+
+  return marked(method.markup)
 })
