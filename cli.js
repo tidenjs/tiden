5d669ee8585dc2d446e4ececf755846b7f907d88
Seke1412 <ngodungcan@gmail.com>
2021-09-19 11:24:15 +0800

    Fix convention

--- cli.js
+++ cli.js
@@ -4,9 +4,12 @@ import parse from "./cli/parse.js"
 import translator from "./cli/translator.js"
 
 try {
-  const [cmd, path, ...args] = parse(...process.argv.slice(2))
-  const resolvePath = translator(path)
-  cmd(path, ...args)
+  const [cmd, ...args] = parse(...process.argv.slice(2))
+
+  translator(args[0]?.path).then(resolvePath => {
+    args[0].path = resolvePath
+    cmd(args)
+  })
 
 } catch (e) {
   process.exit(1)
